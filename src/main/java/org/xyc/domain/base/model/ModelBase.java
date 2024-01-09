664c205bdfcbcc5543a8da1667d5f8589df81d2c
xuyachang <1031372858@qq.com>
2024-01-09 09:34:36 +0800

    新增ModelBase类

--- src/main/java/org/xyc/domain/base/model/ModelBase.java
+++ src/main/java/org/xyc/domain/base/model/ModelBase.java
@@ -1,16 +1,50 @@
 package org.xyc.domain.base.model;
 
+import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-public class ModelBase {
+public class ModelBase<T extends Model<?>> extends Model<T> {
+
+    /**
+     * 主键
+     */
     private Long id;
-    private Long version;
+
+    /**
+     * 版本
+     */
+    private Long _version;
+
+    /**
+     * 是否删除
+     */
     private Integer isDelete;
+
+    /**
+     * 删除时间
+     */
+    private Long deleteAt;
+
+    /**
+     * 创建时间
+     */
     private Date createAt;
+
+    /**
+     * 创建者
+     */
     private Long createBy;
+
+    /**
+     * 更新时间
+     */
     private Date updateAt;
+
+    /**
+     * 更新者
+     */
     private Long updateBy;
 }
